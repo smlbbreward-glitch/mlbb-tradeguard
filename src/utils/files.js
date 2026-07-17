7e5ecb940e59d5b7ddaa1e8437a63ef3c9403205
smlbbreward-glitch <smlbbreward@gmail.com>
2026-07-17 19:55:52 +0800

    Cleanup: gitignore netlify folder, simplify files helper

--- src/utils/files.js
+++ src/utils/files.js
@@ -1,8 +1,3 @@
-export const sanitizeFileName = (name) => {
-  const base = (name || '').replace(/[^a-zA-Z0-9._-]/g, '_').replace(/^\.+/, '') || 'file';
-  if (/^(image|img|photo|picture|screenshot|pic|untitled)\.(png|jpe?g|gif|webp|bmp)$/i.test(base)) {
-    const ext = base.split('.').pop() || 'png';
-    return `upload_${Date.now()}.${ext}`;
-  }
-  return base;
-};
+export function sanitizeFileName(name) {
+  return name.replace(/[^a-zA-Z0-9._-]/g, '_');
+}
